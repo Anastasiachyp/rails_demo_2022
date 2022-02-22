c86813aceaf1a7cf33ad824bd9fcf739f9126901
Anastasiachyp <chypyhanastasia@yahoo.it>
2022-02-22 14:01:06 +0100

    pass all tests

--- spec/request/api/ping_request_spec.rb
+++ spec/request/api/ping_request_spec.rb
@@ -1,7 +1,6 @@
-RSpec.describe Api::PingsController do
+RSpec.describe 'GET /api/pings', type: :request do
   it 'is expected to return a pong message' do
     get '/api/pings'
     expect(response_json['message']).to eq 'pong'
   end
 end
- 
