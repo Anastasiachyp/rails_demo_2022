7b3316d069e2ca466224027803b0eaed1effee62
Anastasiachyp <chypyhanastasia@yahoo.it>
2022-02-22 11:33:06 +0100

    fixed some specs, one still fails

--- spec/requests / api/articles/index_spec.rb
+++ spec/requests / api/articles/index_spec.rb
@@ -1,18 +1,17 @@
 RSpec.describe 'GET /api/articles', type: :request do
   # ARRANGE
-  let!(:articles) { create_list(:articles, 5) }
-
+  let!(:aricles) { create_list(:article, 5) }
   before do
     # ACT
     get '/api/articles'
   end
-
   subject { response }
-
   # ASSERT
   it 'is expected to respond with status code 200' do
     expect(subject.status).to eq 200
   end
+
+  it { is_expected.to have_http_status 200 }
 
   it 'is expected to return a collection of 5 articles' do
     expect(response_json['articles'].size).to eq 5
