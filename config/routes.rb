61a66e73607fd346c3da67e372e13e61f9f82a02
Anastasiachyp <chypyhanastasia@yahoo.it>
2022-02-22 10:46:35 +0100

    WIP adds articles controller and index spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
-    # resources :pings, only: [:index]
-    get 'api/pings', controller: :pings, action: :index
+    resources :pings, only: [:index]
+    get :articles, controller: :articles, action: :index
   end
 end
