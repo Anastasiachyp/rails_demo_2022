2e0db5f8a46f8fb23669d5ca37468b28928dfa7b
Anastasiachyp <chypyhanastasia@yahoo.it>
2022-02-23 10:38:44 +0100

    adds create spec and pass it

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -1,6 +1,5 @@
 class Api::ArticlesController < ApplicationController
   def index
-    # binding.pry
     articles = Article.all
     render json: { articles: articles }
   end
@@ -8,5 +7,13 @@ class Api::ArticlesController < ApplicationController
   def show
     article = Article.find(params[:id])
     render json: { article: article }
+  end
+
+  def create
+    new_article = Article.new
+    new_article.title = params[:article][:title]
+    new_article.body = params[:article][:body]
+    new_article.save
+    render json: { article: new_article }, status: 201
   end
 end
